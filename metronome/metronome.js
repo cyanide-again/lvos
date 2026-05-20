11499f307110bd041fa32bfddc4d6a43b55708a9
cyanide-again <st45.gd@gmail.com>
2026-05-21 07:51:29 +1200

    Fixed accuracy

--- metronome/metronome.js
+++ metronome/metronome.js
@@ -12,7 +12,9 @@ let sleepTime;
 let numerator = 7;
 let denominator = 8;
 let bpm = 120;
-
+let startTime;
+let timeSinceStart;
+let targetTime;
 
 function beep(freq) {
     const osc = audioCtx.createOscillator();
@@ -23,22 +25,28 @@ function beep(freq) {
     osc.stop(audioCtx.currentTime + Math.min(0.1, sleepTime/1000));
 }
 
-
 pausePlay.addEventListener("click", async () => {
     playCount++;
     if (playCount % 2 == 1) {
         pausePlay.innerText = "Pause";
         const prevPlayCount = playCount;
+        startTime = Date.now();
+        targetTime = 0;
 
         while (prevPlayCount == playCount) {
-            beatTime = Math.min(1, numerator - beat);
-            beatDisplay.innerText = beat + 1;
-            // (60/bpm) is the length of each beat in seconds. (4/denominator) calculates each beat's value.
-            // Multiply that by 1000 to convert seconds to ms, and beatTime is for decimal time signatures
-            sleepTime = ((60/bpm) * (4/denominator)) * 1000 * beatTime;
-            beep(beat == 0 ? 880 : 440);
-            beat = (numerator - beat <= 1 ? 0 : beat + 1);
-            await sleep(sleepTime);
+            console.log(timeSinceStart);
+            timeSinceStart = Date.now() - startTime;
+            if (timeSinceStart > targetTime) {
+                beatTime = Math.min(1, numerator - beat);
+                beatDisplay.innerText = beat + 1;
+                // (60/bpm) is the length of each beat in seconds. (4/denominator) calculates each beat's value.
+                // Multiply that by 1000 to convert seconds to ms, and beatTime is for decimal time signatures
+                sleepTime = ((60/bpm) * (4/denominator)) * 1000 * beatTime;
+                beep(beat == 0 ? 880 : 440);
+                beat = (numerator - beat <= 1 ? 0 : beat + 1);
+                targetTime += sleepTime;
+            }
+            await sleep(10);
         }
     } else {
         pausePlay.innerText = "Play";

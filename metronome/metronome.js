506d12d9533e7742512ffdedab70160084bd527d
cyanide-again <st45.gd@gmail.com>
2026-05-20 17:45:05 +1200

    Fixed bugs

--- metronome/metronome.js
+++ metronome/metronome.js
@@ -6,62 +6,58 @@ const bpmInput = document.getElementById("bpmInput");
 const numeratorInput = document.getElementById("numeratorInput");
 const denominatorInput = document.getElementById("denominatorInput");
 let playCount = 0;
-let playing = false;
 let beat = 0;
 let beatTime = 0;
 let sleepTime;
 let numerator = 7;
 let denominator = 8;
-let sleeping;
 let bpm = 120;
+
+
 function beep(freq) {
     const osc = audioCtx.createOscillator();
     osc.connect(audioCtx.destination);
     osc.type = "triangle";
     osc.frequency.setValueAtTime(freq, audioCtx.currentTime);
     osc.start();
-    osc.stop(audioCtx.currentTime + Math.min(0.1, (sleepTime/1000)));
+    osc.stop(audioCtx.currentTime + Math.min(0.1, sleepTime/1000));
 }
+
+
 pausePlay.addEventListener("click", async () => {
-    playCount += 1;
-    if (!playing) {
+    playCount++;
+    if (playCount % 2 == 1) {
         pausePlay.innerText = "Pause";
         const prevPlayCount = playCount;
-        playing = true;
+
         while (prevPlayCount == playCount) {
             beatTime = Math.min(1, numerator - beat);
-            if (beat > numerator) {
-                beat = 0;
-            }
             beatDisplay.innerText = beat + 1;
+            // (60/bpm) is the length of each beat in seconds. (4/denominator) calculates each beat's value.
+            // Multiply that by 1000 to convert seconds to ms, and beatTime is for decimal time signatures
             sleepTime = ((60/bpm) * (4/denominator)) * 1000 * beatTime;
             beep(beat == 0 ? 880 : 440);
-            beat = (numerator - beat < 1 ? 0 : beat + 1);
-            if (sleepTime > 0) await sleep(sleepTime);
+            beat = (numerator - beat <= 1 ? 0 : beat + 1);
+            await sleep(sleepTime);
         }
-    }
-    else {
+    } else {
         pausePlay.innerText = "Play";
         beatDisplay.innerText = "";
         beat = 0;
-        playing = false;
     }
 });
+
 bpmInput.addEventListener("input", () => {
+    if (bpmInput.value <= 0) return;
     bpm = bpmInput.value;
-    if (bpm === "") {
-        bpm = 1;
-    }
 });
+
 numeratorInput.addEventListener("input", () => {
+    if (numeratorInput.value <= 0) return;
     numerator = numeratorInput.value;
-    if (numerator === "") {
-        numerator = 1;
-    }
 });
+
 denominatorInput.addEventListener("input", () => {
+    if (denominatorInput.value <= 0) return;
     denominator = denominatorInput.value;
-    if (denominator === "") {
-        denominator = 1;
-    }
 });
